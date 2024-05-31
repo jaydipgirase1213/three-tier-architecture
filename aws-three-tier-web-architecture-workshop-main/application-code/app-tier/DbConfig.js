4ea68e2cd2db8bc65d3fbd096d07f889c6e5d9f8
Jaydip Girase <140147096+jaydipgirase1213@users.noreply.github.com>
2024-05-31 19:55:26 +0530

    Add files via upload

---
+++ aws-three-tier-web-architecture-workshop-main/application-code/app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'database-1-instance-1.chqe40cwi0dm.ap-southeast-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'mydatabase',
+    DB_DATABASE : 'webappdb'
+});
